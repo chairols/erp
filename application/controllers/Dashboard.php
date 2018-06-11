64ab828a94cf558d832ed7aec4838872989cbc8d
chair <chair@HERNAN-ACER>
2018-06-11 16:37:21 -0300

    - Se agregó Dashboard para corroborar login - bug fixes

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,19 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        
+    }
+
+    function index() {
+        $this->load->view('layout/header');
+        $this->load->view('layout/menu');
+        $this->load->view('dashboard/index');
+        $this->load->view('layout/footer');
+    }
+
+}

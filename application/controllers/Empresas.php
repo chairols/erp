79c064a5b4f230a88bd3ad2d87c80b62e307666f
chair <chair@HERNAN-ACER>
2018-06-17 01:34:05 -0300

    - Se listan los clientes

--- application/controllers/Empresas.php
+++ application/controllers/Empresas.php
@@ -122,6 +122,58 @@ class Empresas extends CI_Controller {
         $this->load->view('empresas/proveedores');
         $this->load->view('layout/footer');
     }
+    
+    function clientes($pagina = 0) {
+        $data['title'] = 'Listado de Clientes';
+        $data['session'] = $this->session->all_userdata();
+        $data['menu'] = $this->r_session->get_menu();
+        $data['javascript'] = array();
+        
+        
+        $per_page = $this->parametros_model->get_valor_parametro_por_usuario('per_page', $data['session']['SID']);
+        $per_page = $per_page['valor'];
+        
+        $where = $this->input->get();
+        $where['empresas.estado'] = 'A';
+        $where['empresas.cliente'] = 'Y';
+        
+        /*
+         * inicio paginador
+         */
+        $total_rows = $this->empresas_model->get_cantidad_where($where);
+        $config['reuse_query_string'] = TRUE;
+        $config['base_url'] = '/empresas/clientes/';
+        $config['total_rows'] = $total_rows;
+        $config['per_page'] = $per_page;
+        $config['first_link'] = '<i class="fa fa-angle-double-left"></i>';
+        $config['first_tag_open'] = '<li>';
+        $config['first_tag_close'] = '</li>';
+        $config['last_link'] = '<i class="fa fa-angle-double-right"></i>';
+        $config['last_tag_open'] = '<li>';
+        $config['last_tag_close'] = '</li>';
+        $config['next_tag_open'] = '<li>';
+        $config['next_tag_close'] = '</li>';
+        $config['prev_tag_open'] = '<li>';
+        $config['prev_tag_close'] = '</li>';
+        $config['cur_tag_open'] = '<li class="active"><a href="#"><b>';
+        $config['cur_tag_close'] = '</b></a></li>';
+        $config['num_tag_open'] = '<li>';
+        $config['num_tag_close'] = '</li>';
+        $this->pagination->initialize($config);
+        $data['links'] = $this->pagination->create_links();
+        $data['total_rows'] = $total_rows;
+        /*
+         * fin paginador
+         */
+        
+        $data['empresas'] = $this->empresas_model->get_cantidad_where_limit($where, $per_page, $pagina);
+        
+        
+        $this->load->view('layout/header', $data);
+        $this->load->view('layout/menu');
+        $this->load->view('empresas/clientes');
+        $this->load->view('layout/footer');
+    }
 }
 
 ?>

a1e8958d385ba6be63fd8045be7e77260deb8592
chair <chair@HERNAN-ACER>
2018-06-17 01:30:02 -0300

    - Se listan los proveedores

--- application/controllers/Empresas.php
+++ application/controllers/Empresas.php
@@ -71,6 +71,57 @@ class Empresas extends CI_Controller {
         $this->load->view('layout/footer');
     }
 
+    function proveedores($pagina = 0) {
+        $data['title'] = 'Listado de Proveedores';
+        $data['session'] = $this->session->all_userdata();
+        $data['menu'] = $this->r_session->get_menu();
+        $data['javascript'] = array();
+        
+        
+        $per_page = $this->parametros_model->get_valor_parametro_por_usuario('per_page', $data['session']['SID']);
+        $per_page = $per_page['valor'];
+        
+        $where = $this->input->get();
+        $where['empresas.estado'] = 'A';
+        $where['empresas.proveedor'] = 'Y';
+        
+        /*
+         * inicio paginador
+         */
+        $total_rows = $this->empresas_model->get_cantidad_where($where);
+        $config['reuse_query_string'] = TRUE;
+        $config['base_url'] = '/empresas/proveedores/';
+        $config['total_rows'] = $total_rows;
+        $config['per_page'] = $per_page;
+        $config['first_link'] = '<i class="fa fa-angle-double-left"></i>';
+        $config['first_tag_open'] = '<li>';
+        $config['first_tag_close'] = '</li>';
+        $config['last_link'] = '<i class="fa fa-angle-double-right"></i>';
+        $config['last_tag_open'] = '<li>';
+        $config['last_tag_close'] = '</li>';
+        $config['next_tag_open'] = '<li>';
+        $config['next_tag_close'] = '</li>';
+        $config['prev_tag_open'] = '<li>';
+        $config['prev_tag_close'] = '</li>';
+        $config['cur_tag_open'] = '<li class="active"><a href="#"><b>';
+        $config['cur_tag_close'] = '</b></a></li>';
+        $config['num_tag_open'] = '<li>';
+        $config['num_tag_close'] = '</li>';
+        $this->pagination->initialize($config);
+        $data['links'] = $this->pagination->create_links();
+        $data['total_rows'] = $total_rows;
+        /*
+         * fin paginador
+         */
+        
+        $data['empresas'] = $this->empresas_model->get_cantidad_where_limit($where, $per_page, $pagina);
+        
+        
+        $this->load->view('layout/header', $data);
+        $this->load->view('layout/menu');
+        $this->load->view('empresas/proveedores');
+        $this->load->view('layout/footer');
+    }
 }
 
 ?>
---
+++ application/views/empresas/proveedores.php
@@ -0,0 +1,74 @@
+<div class="col-md-12">
+    <div class="box">
+        <div class="box-header">
+            <form method="GET" class="input-group input-group-sm col-md-3">
+                <input class="form-control pull-left" name="empresa" placeholder="Empresa" type="text">
+                <div class="input-group-btn">
+                    <button class="btn btn-default" type="submit">
+                        <i class="fa fa-search"></i>
+                    </button>
+                </div>
+            </form>
+            <div class="box-tools">
+                <ul class="pagination pagination-sm no-margin pull-right">
+                    <?= $links ?>
+                </ul>
+            </div>
+        </div>
+        <div class="box-body no-padding">
+            <table class="table table-bordered table-hover table-striped">
+                <tbody>
+                    <tr>
+                        <th>Empresa</th>
+                        <th>CUIT</th>
+                        <th>Tipo</th>
+                        <th>Locación</th>
+                        <th>Actividad</th>
+                        <th>Acciones</th>
+                    </tr>
+                    <?php foreach ($empresas as $empresa) { ?>
+                        <tr>
+                            <td><strong><?= $empresa['idempresa'] ?> - <?= $empresa['empresa'] ?></strong></td>
+                            <td><?= $empresa['cuit'] ?></td>
+                            <td>
+                                <?php if ($empresa['cliente'] == 'Y') { ?>
+                                    <span class="label label-success">Cliente</span>
+                                <?php } ?>
+                                <?php if ($empresa['proveedor'] == 'Y') { ?>
+                                    <span class="label label-warning">Proveedor</span>
+                                <?php } ?>
+                            </td>
+                            <td>
+                                <?php if ($empresa['internacional'] == 'Y') { ?>
+                                    <span class="label label-warning">Internacional</span>
+                                <?php } else { ?>
+                                    <span class="label label-info">Nacional</span>
+                                <?php } ?>
+                            </td>
+                            <td>
+                                <span class="label label-default"><?=$empresa['empresa_tipo']?></span>
+                            </td>
+                            <td>
+                                <a href="#" data-pacement="top" data-toggle="tooltip" data-original-title="Modificar" class="tooltips">
+                                    <button class="btn btn-primary btn-xs">
+                                        <i class="fa fa-edit"></i>
+                                    </button>
+                                </a>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+        <div class="box-footer clearfix">
+            <div class="pull-left">
+                <strong>Total <?= $total_rows ?> registros.</strong>
+            </div>
+            <div class="box-tools">
+                <ul class="pagination pagination-sm no-margin pull-right">
+                    <?= $links ?>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>

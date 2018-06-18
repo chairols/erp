72148905052bb84d6a5a86350beb30a0dad62aa6
chair <chair@HERNAN-ACER>
2018-06-18 15:48:24 -0300

    - Se listan las Líneas

---
+++ application/models/Lineas_model.php
@@ -0,0 +1,39 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Lineas_model extends CI_Model {
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    /*
+     *  Lineas/listar
+     */
+    public function get_cantidad_where($where) {
+        $this->db->select('*');
+        $this->db->from('lineas');
+        $this->db->like($where);
+        
+        $query = $this->db->count_all_results();
+        return $query;
+    }
+    
+    
+    /*
+     *  Lineas/listar
+     */
+    public function gets_where_limit($where, $per_page, $pagina) {
+        $this->db->select('*');
+        $this->db->from('lineas');
+        $this->db->like($where);
+        $this->db->order_by('linea');
+        $this->db->limit($per_page, $pagina);
+        
+        $query = $this->db->get();
+        return $query->result_array();
+    }
+}
+
+?>
---
+++ application/views/lineas/listar.php
@@ -0,0 +1,53 @@
+<div class="col-md-12">
+    <div class="box">
+        <div class="box-header">
+            <form method="GET" class="input-group input-group-sm col-md-5">
+                <input class="form-control pull-left" name="linea" placeholder="Línea" type="text">
+                <div class="input-group-btn">
+                    <button class="btn btn-default" type="submit">
+                        <i class="fa fa-search"></i>
+                    </button>
+                </div>
+            </form>
+            <div class="box-tools">
+                <ul class="pagination pagination-sm no-margin pull-right">
+                    <?= $links ?>
+                </ul>
+            </div>
+        </div>
+        <div class="box-body no-padding">
+            <table class="table table-bordered table-hover table-striped">
+                <tbody>
+                    <tr>
+                        <th>Línea</th>
+                        <th>Nombre Corto</th>
+                        <th>Acciones</th>
+                    </tr>
+                    <?php foreach ($lineas as $linea) { ?>
+                        <tr>
+                            <td><?= $linea['linea'] ?></td>
+                            <td><?= $linea['nombre_corto'] ?></td>
+                            <td>
+                                <a href="/lineas/modificar/<?= $linea['idlinea'] ?>/" data-pacement="top" data-toggle="tooltip" data-original-title="Modificar" class="tooltips">
+                                    <button class="btn btn-primary btn-xs">
+                                        <i class="fa fa-edit"></i>
+                                    </button>
+                                </a>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
+        <div class="box-footer clearfix">
+            <div class="pull-left">
+                <strong>Total <?= $total_rows ?> registros.</strong>
+            </div>
+            <div class="box-tools">
+                <ul class="pagination pagination-sm no-margin pull-right">
+                    <?= $links ?>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>

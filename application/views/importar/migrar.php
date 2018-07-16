16274e1b6903dba1dd50ee35f172c225758ba06b
chair <chair@HERNAN-ACER>
2018-07-15 22:19:34 -0300

    - Bug fixes

--- application/views/importar/migrar.php
+++ application/views/importar/migrar.php
@@ -5,7 +5,7 @@
     <div class="box-body">
         <div class="row">
             <div class="col-xs-8">
-                <strong>Migrar product a articulos (debe existir la tabla product, pero no necesariamente la tabla articulos)</strong>
+                <strong>Migrar <label class="label label-warning">product</label> a <label class="label label-success">articulos</label> (debe existir la tabla product, pero no necesariamente la tabla articulos)</strong>
             </div>
             <div class="col-xs-2" id="boton-1">
                 <button class="btn btn-xs btn-primary" onclick="procesar(1, '/importar/migrar_product_articulos/');">
@@ -19,7 +19,7 @@
         <hr>
         <div class="row">
             <div class="col-xs-8">
-                <strong>Migrar product_abstract a articulos_genericos (debe existir la tabla product_abstract, pero no necesariamente la tabla articulos_genericos)</strong>
+                <strong>Migrar <label class="label label-warning">product_abstract</label> a <label class="label label-success">articulos_genericos</label> (debe existir la tabla product_abstract, pero no necesariamente la tabla articulos_genericos)</strong>
             </div>
             <div class="col-xs-2" id="boton-2">
                 <button class="btn btn-xs btn-primary" onclick="procesar(2, '/importar/migrar_product_abstract_articulos_genericos/');">
@@ -27,6 +27,22 @@
                 </button>
             </div>
             <div class="col-xs-2" id="resultado-2">
+
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-xs-8">
+                <strong>Actualizar tabla <label class="label label-success">articulos</label> mediante TXT de Sistema anterior. <br>
+                    php index.php migrar actualizar_articulos ARTICULO.TXT <br>
+                    curl https://erp.rollerservice.com.ar/migrar/actualizar_articulos/ARCHIVO.TXT (ideal para evitar timeout)
+                    Eliminar cabeceras del archivo TXT
+                </strong>
+            </div>
+            <div class="col-xs-2" id="boton-3">
+                <label class="label label-warning">Debe ejecutarse por consola</label>
+            </div>
+            <div class="col-xs-2" id="resultado-3">
 
             </div>
         </div>

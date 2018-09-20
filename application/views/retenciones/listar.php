f85d042f24dba7b1e79ce83e7e9648233fc3174c
chair <chair@HERNAN-ACER>
2018-09-20 12:12:23 -0300

    - Se agrega buscador al listado de retenciones

--- application/views/retenciones/listar.php
+++ application/views/retenciones/listar.php
@@ -1,5 +1,13 @@
 <div class="box">
     <div class="box-header">
+        <form method="GET" class="input-group input-group-sm col-md-5">
+            <input class="form-control pull-left" name="proveedor" id="proveedor" placeholder="Buscar ..." type="text">
+            <div class="input-group-btn">
+                <button class="btn btn-default" type="submit">
+                    <i class="fa fa-search"></i>
+                </button>
+            </div>
+        </form>
         <div class="box-tools">
             <ul class="pagination pagination-sm no-margin pull-right">
                 <?= $links ?>
@@ -18,12 +26,12 @@
                 </tr>
                 <?php foreach ($retenciones as $retencion) { ?>
                     <tr>
-                        <td><?= str_pad($retencion['punto'], 4, '0', STR_PAD_LEFT)?>-<?=str_pad($retencion['numero'], 8, '0', STR_PAD_LEFT)?></td>
-                        <td><?=$retencion['proveedor']?></td>
-                        <td><?=$retencion['fecha']?></td>
-                        <td><?=$retencion['idjurisdiccion_afip']?> - <?=$retencion['jurisdiccion']?></td>
+                        <td><?= str_pad($retencion['punto'], 4, '0', STR_PAD_LEFT) ?>-<?= str_pad($retencion['numero'], 8, '0', STR_PAD_LEFT) ?></td>
+                        <td><?= $retencion['proveedor'] ?></td>
+                        <td><?= $retencion['fecha'] ?></td>
+                        <td><?= $retencion['idjurisdiccion_afip'] ?> - <?= $retencion['jurisdiccion'] ?></td>
                         <td>
-                            
+
                         </td>
                     </tr>
                 <?php } ?>

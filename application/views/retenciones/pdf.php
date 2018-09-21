9d0a0dccb375f483ded4877e4d08790061d24e76
chair <chair@HERNAN-ACER>
2018-09-21 13:25:07 -0300

    - Se corrige formateo de bases imponibles en PDF de retenciones

--- application/views/retenciones/pdf.php
+++ application/views/retenciones/pdf.php
@@ -41,7 +41,7 @@
         <tr>
             <td style="width: 20%; text-align: right" class="bg-gray"><?=str_pad($item['punto_de_venta'], 4, '0', STR_PAD_LEFT)?>-<?=str_pad($item['comprobante'], 8, '0', STR_PAD_LEFT)?></td>
             <td style="width: 20%; text-align: right"><?=$item['fecha_formateada']?></td>
-            <td style="width: 20%; text-align: right"><?=$item['base_imponible']?></td>
+            <td style="width: 20%; text-align: right"><?=number_format($item['base_imponible'], 2)?></td>
             <td style="width: 20%; text-align: right"><?=$retencion['alicuota']?> %</td>
             <td style="width: 20%; text-align: right"><?=number_format(round((($item['base_imponible']*$retencion['alicuota'])/100), 2), 2)?></td>
             <?php $total_base_imponible += $item['base_imponible']; ?>    

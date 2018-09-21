eb3768708d44c8c5d07a3bb1a583d5b0c30a6cc0
chair <chair@HERNAN-ACER>
2018-09-21 10:52:44 -0300

    - bug fixes

--- application/controllers/Retenciones.php
+++ application/controllers/Retenciones.php
@@ -382,7 +382,7 @@ class Retenciones extends CI_Controller {
         }
         
         // create new PDF document
-        $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+        $pdf = new Tcpdf(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
         // set document information
         $pdf->SetCreator(PDF_CREATOR);

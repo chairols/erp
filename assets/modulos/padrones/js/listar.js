b468b20fb44a6f90758cbaec6e565c23560717b2
chair <chair@HERNAN-ACER>
2018-10-25 16:51:28 -0300

    - Se muestra alerta previa a borrar un padrón

--- assets/modulos/padrones/js/listar.js
+++ assets/modulos/padrones/js/listar.js
@@ -3,38 +3,43 @@ $(".borrar_padron").click(function() {
         'idpadron': this.attributes.idpadron.value
     };
     
-    $.ajax({
-        type: 'POST',
-        url: '/padrones/borrar_ajax/',
-        data: datos,
-        beforeSend: function () {
-        
-        },
-        success: function (data) {
-            resultado = $.parseJSON(data);
-            if (resultado['status'] == 'error') {
-                $.notify('<strong>' + resultado['data'] + '</strong>',
-                        {
-                            type: 'danger',
-                            allow_dismiss: false
-                        });
-            } else if (resultado['status'] == 'ok') {
-                $.notify('<strong>' + resultado['data'] + '</strong>',
-                        {
-                            type: 'success',
-                            allow_dismiss: false
-                        });
-            }
-        },
-        error: function (xhr) { // if error occured
-            $("#body-tabla-items").html(xhr.responseText);
-            console.log(xhr);
+    alertify.confirm("Se eliminará el padrón del proveedor<br><strong>¿Desea confirmar?</strong>", function (e) {
+        if (e) {
+            $.ajax({
+                type: 'POST',
+                url: '/padrones/borrar_ajax/',
+                data: datos,
+                beforeSend: function () {
 
-            $.notify('<strong>Ha ocurrido el siguiente error:</strong><br>' + xhr.statusText,
-                    {
-                        type: 'danger',
-                        allow_dismiss: false
-                    });
+                },
+                success: function (data) {
+                    resultado = $.parseJSON(data);
+                    if (resultado['status'] == 'error') {
+                        $.notify('<strong>' + resultado['data'] + '</strong>',
+                                {
+                                    type: 'danger',
+                                    allow_dismiss: false
+                                });
+                    } else if (resultado['status'] == 'ok') {
+                        $.notify('<strong>' + resultado['data'] + '</strong>',
+                                {
+                                    type: 'success',
+                                    allow_dismiss: false
+                                });
+                    }
+                },
+                error: function (xhr) { // if error occured
+                    $("#body-tabla-items").html(xhr.responseText);
+                    console.log(xhr);
+
+                    $.notify('<strong>Ha ocurrido el siguiente error:</strong><br>' + xhr.statusText,
+                            {
+                                type: 'danger',
+                                allow_dismiss: false
+                            });
+                }
+            });
         }
     });
+    
 });

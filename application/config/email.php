c7dc13c93cd9597063e9d2ca61f8b3e41d7bb781
chair <chair@HERNAN-ACER>
2018-12-12 13:38:35 -0300

    - Se quita debugger

--- application/config/email.php
+++ application/config/email.php
@@ -1,5 +1,9 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed.');
-
+/*
+ *  Para poder enviar archivos con adjuntos en el WHM en MultiPHP Editor habilitar
+ *  - allow_url_fopen
+ *  - allow_url_include
+ */
 $config['useragent']        = 'PHPMailer';              // Mail engine switcher: 'CodeIgniter' or 'PHPMailer'
 $config['protocol']         = 'smtp';                   // 'mail', 'sendmail', or 'smtp'
 $config['mailpath']         = '/usr/sbin/sendmail';
@@ -10,7 +14,7 @@ $config['smtp_pass']        = 'soccer2000';
 $config['smtp_port']        = 465;
 $config['smtp_timeout']     = 30;                       // (in seconds)
 $config['smtp_crypto']      = 'ssl';                       // '' or 'tls' or 'ssl'
-$config['smtp_debug']       = 4;                        // PHPMailer's SMTP debug info level: 0 = off, 1 = commands, 2 = commands and data, 3 = as 2 plus connection status, 4 = low level data output.
+$config['smtp_debug']       = 0;                        // PHPMailer's SMTP debug info level: 0 = off, 1 = commands, 2 = commands and data, 3 = as 2 plus connection status, 4 = low level data output.
 $config['debug_output']     = '';                       // PHPMailer's SMTP debug output: 'html', 'echo', 'error_log' or user defined function with parameter $str and $level. NULL or '' means 'echo' on CLI, 'html' otherwise.
 $config['smtp_auto_tls']    = true;                     // Whether to enable TLS encryption automatically if a server supports it, even if `smtp_crypto` is not set to 'tls'.
 $config['smtp_conn_options'] = array();                 // SMTP connection options, an array passed to the function stream_context_create() when connecting via SMTP.

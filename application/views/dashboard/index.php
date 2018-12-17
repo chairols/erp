d5f023a62a1a1e1ed1941ec61c558d8da11bbd7b
chair <chair@HERNAN-ACER>
2018-12-17 08:51:56 -0300

    - Muestra link a commits en github

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -29,5 +29,22 @@
             </div>
         </div>
     </div>
+    <div class="col-md-3 col-sm-6 col-xs-12">
+        <div class="info-box bg-black">
+            <span class="info-box-icon">
+                <i class="fa fa-github"></i>
+            </span>
+            <div class="info-box-content">
+                <span class="info-box-text">Commits</span>
+                <span class="info-box-number">&nbsp;</span>
+                <div class="progress">
+                    <div class="progress-bar" style="width: 100%"></div>
+                </div>
+                <span class="progress-description">
+                    <a href="https://github.com/chairols/erp/commits/master" target="_blank">Ver en Github</a>
+                </span>
+            </div>
+        </div>
+    </div>
 </div>
     
